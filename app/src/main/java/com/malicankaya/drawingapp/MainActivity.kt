00771b9e0e016be1afde67f841513bb64f5a583f
malicankaya <malicankaya97@gmail.com>
2022-09-18 23:48:27 +0300

    adding file permission request

--- app/src/main/java/com/malicankaya/drawingapp/MainActivity.kt
+++ app/src/main/java/com/malicankaya/drawingapp/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.malicankaya.drawingapp
 
+import android.Manifest
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
 import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 
@@ -14,11 +20,38 @@ class MainActivity : AppCompatActivity() {
     private var drawingView: DrawingView? = null
     private var mImageButtonCurrentPaint: ImageButton? = null
 
+    private val requestPermissions: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            permissions.entries.forEach {
+                val permission = it.key
+                val isGranted = it.value
+
+                if (isGranted) {
+                    if (permission == Manifest.permission.READ_EXTERNAL_STORAGE) {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Permission granted now you can read the storage files",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        if (permission == Manifest.permission.READ_EXTERNAL_STORAGE) {
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Permission denied, you cannot read the storage files",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                }
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val ibBrush: ImageButton = findViewById(R.id.ibBrush)
         val linearLayoutColors = findViewById<LinearLayout>(R.id.llBrushColor)
+        val ibGallery: ImageButton = findViewById(R.id.ibGallery)
 
         mImageButtonCurrentPaint = linearLayoutColors[2] as ImageButton
         mImageButtonCurrentPaint!!.setImageDrawable(
@@ -30,6 +63,10 @@ class MainActivity : AppCompatActivity() {
 
         ibBrush.setOnClickListener {
             showSetBrushSizeChooserDialog()
+        }
+
+        ibGallery.setOnClickListener {
+            requestStoragePermission()
         }
     }
 
@@ -68,6 +105,36 @@ class MainActivity : AppCompatActivity() {
             mImageButtonCurrentPaint?.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.pallet_normal))
             mImageButtonCurrentPaint = view
         }
+    }
+
+    private fun requestStoragePermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+        )){
+            showRationaleDialog("Drawing App", "Drawing App needs to access your external storage.")
+        }else{
+            requestPermissions.launch(arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ))
+        }
+    }
+
+    private fun showRationaleDialog(title: String, message: String){
+        val builder = AlertDialog.Builder(this)
+
+        builder.setTitle(title)
+        builder.setMessage(message)
+        builder.setNegativeButton("Cancel"){ dialog, _->
+            dialog.dismiss()
+        }
+        builder.setPositiveButton("OK"){ dialog, _->
+            dialog.dismiss()
+            requestPermissions.launch(arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE))
+        }
+
+        builder.create().show()
     }
 
 }
